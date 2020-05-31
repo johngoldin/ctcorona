0fb4a3d3cb734f0c8d5e3daf07b090d739850102
John Goldin <john.r.goldin@gmail.com>
2020-05-30 20:19:58 -0400

    created function to corral census info

--- daily_ct_stats.R
+++ daily_ct_stats.R
@@ -17,13 +17,6 @@ library(RcppRoll)
 
 path_to_post <- "~/Dropbox/Programming/R_Stuff/can_i_blog_too/content/post/2020-03-29-covid19-cases-in-connecticut/"
 path_to_ctcorona <- "~/Documents/R_local_repos/ctcorona/"
-vars_65_plus <- paste0("B01001_0", c(20:25, 44:49))
-vars_under_25 <- c(paste0("B01001_00", c(3:9)), paste0("B01001_0", c(10, 27:34)))
-vars_enrolled_student <- paste0("B14004_0", c("03", "08", "19", "24"))
-# B19013001 is median household income CPI adjusted dollars
-# B11001001 is total number of households
-
-vars_poverty <- "B17001002" # B17001001 is total
 
 if (!exists("county_geometries")) load(paste0(path_to_ctcorona, "census_population.RData"))
 if (!exists("county_geometries")) {
@@ -36,92 +29,167 @@ if (!exists("county_geometries")) {
   # save(county_geometries, census_population, file = paste0(path_to_ctcorona, "census_population.RData"))
 }
 if (!exists("town_geometries")) {
-  vars <- load_variables(2017, "acs5", cache = TRUE) %>%
-  mutate(table_id = str_sub(name, 1, 6),
-         # Race generally is in parentheses after the concept name.
-         # But for a few cases, something else is in parentheses first. So I
-         # am going to blank out that stuff and then assume whatever I find inside
-         # of parentheses is race.
-         concept = str_replace_all(concept,
-           c("\\(IN 2017 INFLATION-ADJUSTED DOLLARS\\)" = "",
-             "\\(EXCLUDING HOUSEHOLDERS, SPOUSES, AND UNMARRIED PARTNERS\\)" = "",
-             "\\(SSI\\)" = "",
-             "\\(INCLUDING LIVING ALONE\\)" = "",
-             "\\(IN MINUTES\\)" = "",
-             "\\(DOLLARS\\)" = "",
-             "\\(CT, ME, MA, MI, MN, NH, NJ, NY, PA, RI, VT, WI\\)" = "--CT, ME, MA, MI, MN, NH, NJ, NY, PA, RI, VT, WI--",
-             "\\(CAR, TRUCK, OR VAN\\)" = "--CAR, TRUCK, OR VAN--",
-             "\\(\\)" = ""
-         )),
-         race = str_extract(concept, "\\(.+\\)"),
-         race = str_replace(race, "\\(", ""),
-         race = str_replace(race, "\\)", ""))
-         # I should have been able to do this in one line, but it doesn't seem to work:
-         # race = str_extract(concept, "\\((.*?)\\)"))
-B17010_variables <- vars %>%
-  filter(table_id == "B17010", is.na(race)) %>%
-  pluck("name")
+#   acs_vars <- load_variables(2017, "acs5", cache = TRUE) %>%
+#   mutate(table_id = str_sub(name, 1, 6),
+#          # Race generally is in parentheses after the concept name.
+#          # But for a few cases, something else is in parentheses first. So I
+#          # am going to blank out that stuff and then assume whatever I find inside
+#          # of parentheses is race.
+#          concept = str_replace_all(concept,
+#            c("\\(IN 2017 INFLATION-ADJUSTED DOLLARS\\)" = "",
+#              "\\(EXCLUDING HOUSEHOLDERS, SPOUSES, AND UNMARRIED PARTNERS\\)" = "",
+#              "\\(SSI\\)" = "",
+#              "\\(INCLUDING LIVING ALONE\\)" = "",
+#              "\\(IN MINUTES\\)" = "",
+#              "\\(DOLLARS\\)" = "",
+#              "\\(CT, ME, MA, MI, MN, NH, NJ, NY, PA, RI, VT, WI\\)" = "--CT, ME, MA, MI, MN, NH, NJ, NY, PA, RI, VT, WI--",
+#              "\\(CAR, TRUCK, OR VAN\\)" = "--CAR, TRUCK, OR VAN--",
+#              "\\(\\)" = ""
+#          )),
+#          race = str_extract(concept, "\\(.+\\)"),
+#          race = str_replace(race, "\\(", ""),
+#          race = str_replace(race, "\\)", ""))
+#          # I should have been able to do this in one line, but it doesn't seem to work:
+#          # race = str_extract(concept, "\\((.*?)\\)"))
+# B17010_variables <- vars %>%
+#   filter(table_id == "B17010", is.na(race)) %>%
+#   pluck("name")
+  test_function <- function(df, var_name = item){
+    df %>%
+      mutate({{var_name}} := 1, "{{ var_name }}_two" := 2)
+  }
+  test_function(college_plus_state_acs, var_name = stuff)
 
-  # town_geometries <- tigris::county_subdivisions(state = "CT", cb = FALSE)
-  # census_population_town <- get_acs(geography = "county subdivision",
-  #                              variables = "B01003_001",
-  #                              state = "CT",
-  #                              geometry = FALSE) %>%
-  #   mutate(town = str_replace(NAME, " town, .* County, Connecticut", ""))
-  age_state_acs <- get_acs(geography = "state",
-                          state = "CT",
-                          # county = "New Haven",
-                          geometry = "FALSE", # no map at this time
-                          survey = "acs5",
-                          variables = vars_65_plus,
-                          summary_var = "B01001_001") %>%
-    filter(estimate > 0) %>%
-    group_by(GEOID) %>%
-    summarize(age65plus = sum(estimate),
-              age65plus_moe = moe_sum(moe, estimate),
-              total_pop = max(summary_est),
-              total_pop_moe = max(summary_moe)
-    ) %>%
-    mutate(pct65plus = age65plus / total_pop)
-  age_town_acs <- get_acs(geography = "county subdivision",  # for CT, that means towns
-                          state = "CT",
-                          # county = "New Haven",
-                          geometry = "FALSE", # no map at this time
-                          survey = "acs5",
-                          variables = vars_65_plus,
-                          summary_var = "B01001_001") %>%
-    filter(estimate > 0) %>%
-    mutate(town = str_replace(NAME, " town, .* County, Connecticut", "")) %>%
-    group_by(GEOID, town) %>%
-    summarize(age65plus = sum(estimate),
-              age65plus_moe = moe_sum(moe, estimate),
-              total_pop = max(summary_est),
-              total_pop_moe = max(summary_moe)
-    ) %>%
-    mutate(pct65plus = age65plus / total_pop)
-  town_geometries <- tigris::county_subdivisions(state = "CT", cb = FALSE) %>%
+  # see this article to glue together var names: https://www.tidyverse.org/blog/2020/02/glue-strings-and-tidy-eval/
+  var_acs <- function(variables, var_for_summary, var_name = item, as_pct = TRUE, geography) {
+       get_acs(geography = geography,
+                             state = "CT",
+                             geometry = "FALSE", # no map at this time
+                             survey = "acs5",
+                             variables = variables,
+                             summary_var = var_for_summary) %>%
+      filter(estimate > 0) %>%
+      group_by(NAME, GEOID) %>%
+      summarize("{{ var_name }}" := sum(estimate),
+                moe := moe_sum(moe, estimate),
+                total_pop = max(summary_est),
+                total_pop_moe = max(summary_moe)
+      ) %>%
+     mutate(NAME = str_replace(NAME, " town, .* County, Connecticut", "")) %>%
+     mutate(NAME = str_replace(NAME, " County, Connecticut", "")) %>%
+     # mutate("{{var_name}}_pct" := {{ var_name }} / total_pop) %>%
+     mutate(item_pct := {{ var_name }} / total_pop,
+            item_pct_moe := moe_ratio({{var_name}}, total_pop, moe, total_pop_moe)) %>%
+      select(GEOID, NAME, {{var_name}}, moe, item_pct, item_pct_moe, total_pop) %>%
+      rename("{{var_name}}_moe" := moe,
+             "{{var_name}}_pct" := item_pct, "{{var_name}}_pct_moe" := item_pct_moe)
+  }
+  # test2 <- var_acs(variables = vars_65_plus, var_for_summary = "B01001_001", geography = "county", var_name = age_65_plus)
+  # test <- get_acs(variables = vars_65_plus, geography = "county", survey = "acs5", geometry = FALSE, summary_var = "B01001_001")
+vars_65_plus <- paste0("B01001_0", c(20:25, 44:49))
+vars_under_25 <- c(paste0("B01001_00", c(3:9)), paste0("B01001_0", c(10, 27:34)))
+vars_enrolled_student <- paste0("B14004_0", c("03", "08", "19", "24"))
+# B19013001 is median household income CPI adjusted dollars
+# B11001001 is total number of households
+vars_college_plus <- c("B15003_022", "B15003_023", "B15003_024", "B15003_025") # total B15003_001   college or grad, 25 years and older
+get_acs(geography = "state",
+                             state = "CT",
+                             geometry = "FALSE", # no map at this time
+                             survey = "acs5",
+                             variables = "B17001_001",
+                             summary_var = NULL)
+
+vars_poverty <- "B17001_002" # "B17001_001" is total
+  college_plus_state_acs <- var_acs(variables = vars_college_plus, var_for_summary = "B15003_001",
+                           geography = "state", var_name = college_plus)
+  college_plus_county_acs <- var_acs(variables = vars_college_plus, var_for_summary = "B15003_001",
+                           geography = "county", var_name = college_plus)
+  college_plus_town_acs <- var_acs(variables = vars_college_plus, var_for_summary = "B15003_001",
+                           geography = "county subdivision", var_name = college_plus)
+
+  poverty_state_acs <- var_acs(variables = "B17001_002", var_for_summary = "B17001_001",
+                           geography = "state", var_name = poverty)
+  poverty_county_acs <- var_acs(variables = "B17001_002", var_for_summary = "B17001_001",
+                           geography = "county", var_name = poverty)
+  poverty_town_acs <- var_acs(variables = "B17001_002", var_for_summary = "B17001_001",
+                           geography = "county subdivision", var_name = poverty)
+
+  hh_inc_state_acs <- var_acs(variables = "B19013_001", var_for_summary = "B19013_001",
+                           geography = "state", var_name = hh_income) %>%
+    select(-hh_income_pct, -hh_income_pct_moe, -total_pop)
+  hh_inc_county_acs <- var_acs(variables = "B19013_001", var_for_summary = "B19013_001",
+                           geography = "county", var_name = hh_income)  %>%
+    select(-hh_income_pct, -hh_income_pct_moe, -total_pop)
+  hh_inc_town_acs <- var_acs(variables = "B19013_001", var_for_summary = "B19013_001",
+                           geography = "county subdivision", var_name = hh_income)  %>%
+    select(-hh_income_pct, -hh_income_pct_moe, -total_pop)
+  old_65_state_acs <- var_acs(variables = vars_65_plus, var_for_summary = "B01001_001",
+                           geography = "state", var_name = age_65_plus)
+  old_65_county_acs <- var_acs(variables = vars_65_plus, var_for_summary = "B01001_001",
+                           geography = "county", var_name = age_65_plus)
+  old_65_town_acs <- var_acs(variables = vars_65_plus, var_for_summary = "B01001_001",
+                           geography = "county subdivision", var_name = age_65_plus)
+  young_state_acs <- var_acs(variables = vars_under_25, var_for_summary = "B01001_001",
+                           geography = "state", var_name = under_25)
+  young_county_acs <- var_acs(variables = vars_under_25, var_for_summary = "B01001_001",
+                           geography = "county", var_name = under_25)
+  young_town_acs <- var_acs(variables = vars_under_25, var_for_summary = "B01001_001",
+                           geography = "county subdivision", var_name = under_25)
+  # of population over 25, percent college degree or higher
+  college_state_acs <- var_acs(variables = vars_enrolled_student, var_for_summary = "B01001_001",
+                           geography = "state", var_name = coll_student)
+  college_county_acs <- var_acs(variables = vars_enrolled_student, var_for_summary = "B01001_001",
+                           geography = "county", var_name = coll_student)
+  college_town_acs <- var_acs(variables = vars_enrolled_student, var_for_summary = "B01001_001",
+                           geography = "county subdivision", var_name = coll_student)
+  state_info <- college_plus_state_acs %>%
+    select(-total_pop) %>%
+    left_join(poverty_state_acs %>% select(-total_pop),
+              by = c("GEOID", "NAME")) %>%
+    left_join(hh_inc_state_acs,
+              by = c("GEOID", "NAME")) %>%
+    left_join(old_65_state_acs,
+              by = c("GEOID", "NAME")) %>%
+    left_join(young_state_acs %>% select(-total_pop),
+              by = c("GEOID", "NAME")) %>%
+    left_join(college_state_acs %>% select(-total_pop),
+              by = c("GEOID", "NAME"))
+  county_info <- college_plus_county_acs %>%
+    select(-total_pop) %>%
+    left_join(poverty_county_acs %>% select(-total_pop),
+              by = c("GEOID", "NAME")) %>%
+    left_join(hh_inc_county_acs,
+              by = c("GEOID", "NAME")) %>%
+    left_join(old_65_county_acs,
+              by = c("GEOID", "NAME")) %>%
+    left_join(young_county_acs %>% select(-total_pop),
+              by = c("GEOID", "NAME")) %>%
+    left_join(college_county_acs %>% select(-total_pop),
+              by = c("GEOID", "NAME"))
+  town_info <- college_plus_town_acs %>%
+    select(-total_pop) %>%
+    left_join(poverty_town_acs %>% select(-total_pop),
+              by = c("GEOID", "NAME")) %>%
+    left_join(hh_inc_town_acs,
+              by = c("GEOID", "NAME")) %>%
+    left_join(old_65_town_acs,
+              by = c("GEOID", "NAME")) %>%
+    left_join(young_town_acs %>% select(-total_pop),
+              by = c("GEOID", "NAME")) %>%
+    left_join(college_town_acs %>% select(-total_pop),
+              by = c("GEOID", "NAME"))
+  ggplot(data = town_geometries, aes(x = density, total_pop)) + geom_text(aes(label = NAME))
+town_geometries <- tigris::county_subdivisions(state = "CT", cb = FALSE) %>%
                 filter(NAME != "County subdivisions not defined") %>%
-    mutate(area =  st_area(geometry) / 2.59e+6) %>%
-    left_join(age_town_acs, by = "GEOID")
+  select(-NAME) %>%
+    left_join(town_info, by = "GEOID") %>%
+    rename(town = NAME) %>%
+    mutate(density = total_pop / (ALAND / 2589988.1103))
   # save(county_geometries, town_geometries, census_population, age_state_acs, file = paste0(path_to_ctcorona, "census_population.RData"))
 }
 # ggplot(data = town_geometries + geom_sf(aes(fill = pct65plus))
 
-age_county_acs <- get_acs(geography = "county",  # for CT, that means towns
-                          state = "CT",
-                          # county = "New Haven",
-                          geometry = "FALSE", # no map at this time
-                          survey = "acs5",
-                          variables = vars_65_plus,
-                          summary_var = "B01001_001") %>%
-  filter(estimate > 0) %>%
-  mutate(COUNTY = str_replace(NAME, " County, Connecticut", "")) %>%
-  group_by(GEOID, COUNTY) %>%
-  summarize(age65plus = sum(estimate),
-            age65plus_moe = moe_sum(moe, estimate),
-            total_pop = max(summary_est, na.rm = TRUE),
-            total_pop_moe = NA_real_ # max(summary_moe, na.rm = TRUE)
-  )
+
 
 
 if (!exists("nyt_series")) load(paste0(path_to_ctcorona, "nyt_series.RData"))
