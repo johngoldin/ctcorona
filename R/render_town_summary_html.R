b2bef27abf2eb1c48b37340422a156cd47b36640
John Goldin <john.r.goldin@gmail.com>
2020-06-26 08:28:44 -0400

    rendering the county summary pages

--- R/render_town_summary_html.R
+++ R/render_town_summary_html.R
@@ -1,21 +1,25 @@
 # render_report.r file (note the use of file.path() to avoid any dependency on the operative system :wink: )
 # based on this example: https://community.rstudio.com/t/is-it-possible-to-save-the-html-output-in-a-directory-which-is-not-the-one-where-the-rmd-file-resides/3588/12
 library(rmarkdown)
-code_dir <- "code/2020-06-07-test-post-two"
+code_dir <- "code"
 county = "New Haven"
-report_filename <- paste0("county", "_summary.Rmd")
-report_filename <- file.path(code_dir, report_filename)
-output_dir <- path_to_static_june
-output <- file.path("..", output_dir)
-render(report_filename,
-       output_dir = output_dir,
-       output_file = paste0(county %>% tolower() %>% str_replace(" ", "_"), "_summary.html"),
-       params = list(output_dir = output, county = county))
+report_filename <- paste0(file.path(getwd(), code_dir, "county_summary.Rmd"))
+output_dir <- paste0(path_to_static_june, "index.en_files/")
 
-#myreport.Rmd file
+the_counties <- c("Fairfield",  "Hartford",   "Litchfield",
+                  "Middlesex",  "New Haven",  "New London",
+                  "Tolland",    "Windham" )
 
-render(report_filename,
-       output_dir = output_dir,
-       output_file = paste0(county %>% tolower() %>% str_replace(" ", "_"), "_summary.html"),
-       params = list(output_dir = output, county = county))
+render_county <- function(acounty) {
+        render(report_filename,
+               output_dir = output_dir,
+               output_file = paste0(acounty %>% tolower() %>% str_replace(" ", "_"), "_summary.html"),
+               params = list(output_dir = output_dir, county = acounty))
+        print(paste("Created html file for", acounty))
+}
+
+
+walk(the_counties, render_county)
+
+
 
